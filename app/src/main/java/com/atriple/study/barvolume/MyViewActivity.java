195a6ca3a09ead5b7cf26b2e9a4859cfe1ca1496
atriple <andiaqil.dpe@gmail.com>
2019-12-04 12:37:38 +0700

    change title on MyViewActivity, edit manifest file

--- app/src/main/java/com/atriple/study/barvolume/MyViewActivity.java
+++ app/src/main/java/com/atriple/study/barvolume/MyViewActivity.java
@@ -10,5 +10,9 @@ public class MyViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
     }
 }
