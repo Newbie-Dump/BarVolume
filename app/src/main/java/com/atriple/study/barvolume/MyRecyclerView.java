a9eb11b8d1060b6693cef409d388987cdbed3a7b
atriple <andiaqil.dpe@gmail.com>
2019-12-06 21:50:34 +0700

    Add Card View and Action Bar

--- app/src/main/java/com/atriple/study/barvolume/MyRecyclerView.java
+++ app/src/main/java/com/atriple/study/barvolume/MyRecyclerView.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.atriple.study.barvolume.adapter.CardViewHeroAdapter;
 import com.atriple.study.barvolume.adapter.GridHeroAdapter;
 import com.atriple.study.barvolume.adapter.ListHeroAdapter;
 import com.atriple.study.barvolume.model.Hero;
@@ -19,11 +20,13 @@ import java.util.ArrayList;
 public class MyRecyclerView extends AppCompatActivity {
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recycler_view);
+        setActionBarTitle("Mode List");
 
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
@@ -44,6 +47,18 @@ public class MyRecyclerView extends AppCompatActivity {
         rvHeroes.setAdapter(gridHeroAdapter);
     }
 
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -59,14 +74,19 @@ public class MyRecyclerView extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                title = "Mode List";
                 showRecyclerList();
                 break;
             case R.id.action_grid:
+                title = "Mode Grid";
                 showRecyclerGrid();
                 break;
             case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecyclerCardView();
                 break;
         }
+        setActionBarTitle(title);
     }
 
 }
