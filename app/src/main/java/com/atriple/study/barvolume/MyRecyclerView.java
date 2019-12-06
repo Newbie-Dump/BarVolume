224411d3b015c7c40bd25432dc3b12130c5d548f
atriple <andiaqil.dpe@gmail.com>
2019-12-06 16:23:29 +0700

    Implement model to activity

--- app/src/main/java/com/atriple/study/barvolume/MyRecyclerView.java
+++ app/src/main/java/com/atriple/study/barvolume/MyRecyclerView.java
@@ -1,14 +1,36 @@
 package com.atriple.study.barvolume;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.atriple.study.barvolume.adapter.ListHeroAdapter;
+import com.atriple.study.barvolume.model.Hero;
+import com.atriple.study.barvolume.model.HeroesData;
+
+import java.util.ArrayList;
+
 public class MyRecyclerView extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recycler_view);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
     }
 }
